95731429aee678174bfff44301d6bf244c056dfc
minasangG <cwrcabaron@gmail.com>
2023-02-20 01:00:34 +0800

    Test Commit February 20 VS Code

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let iceCream = "bar";
+
+if (iceCream === "chocolate") {
+    alert("Yay this is Ice cream!")
+} else {
+    alert("Oh no! This is not an ice cream!")
+}
+
+console.log("This is working!!")
